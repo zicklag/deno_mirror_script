4878ae60ec1cee0e356a1019c33d45b639aac21f
Zicklag <zicklag@katharostech.com>
2024-04-08 12:00:22 -0500

    feat: implement initial checksum checker script.

--- main.ts
+++ main.ts
@@ -16,19 +16,17 @@ import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 import { assert } from "https://deno.land/std@0.221.0/assert/assert.ts";
 
 await new Command()
-  .name("s3-sync")
+  .name("s3-checksum-compare")
   .version("0.1.0")
-  .description("S3 sync command")
-  .option(
-    "-E, --env-auth",
-    "Use environment variables for auth, instead of the ~/.aws/credentials file."
+  .description(
+    "Compares the checksums of an S3 bucket with the files in a local dir to make sure they are the same."
   )
-  .arguments("<bucket:string>")
-  .action(async (opts, bucket) => {
+  .arguments("<bucket:string> <dir:string>")
+  .action(async (_opts, bucket, dir) => {
     let accessKey;
     let secretKey;
 
-    if (opts.envAuth) {
+    if (Deno.env.get("AWS_ACCESS_KEY_ID")) {
       accessKey = Deno.env.get("AWS_ACCESS_KEY_ID");
       secretKey = Deno.env.get("AWS_SECRET_ACCESS_KEY");
     } else {
@@ -46,11 +44,7 @@ await new Command()
     if (!(accessKey && secretKey)) {
       console.error("Missing access key or secret key.");
       console.error(
-        `Attempted to read auth from ${
-          opts.envAuth
-            ? "environment variables: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY."
-            : "~/.aws/credentials default profile."
-        }`
+        `Attempted to read auth from environment variables: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY. and then the ~/.aws/credentials default profile.`
       );
       Deno.exit(1);
     }
@@ -61,8 +55,6 @@ await new Command()
           accessKeyId: accessKey,
           secretAccessKey: secretKey,
         },
-        bucketEndpoint: "s3.us-east-1.amazonaws.com",
-        region: "us-east-1",
       },
     ]);
 
@@ -78,11 +70,10 @@ await new Command()
         while (isTruncated) {
           const { Contents, IsTruncated, NextContinuationToken } =
             await s3client.send(command);
-          console.log(
-            `    Received batch of ${(Contents || []).length} objects.`
-          );
           // Note: we check for a non-null size to filter out directories.
-          objs = [...objs, ...(Contents || []).filter((x) => !!x.Size)];
+          const files = (Contents || []).filter((x) => !!x.Size);
+          console.log(`    Received batch of ${files.length} objects.`);
+          objs = [...objs, ...files];
           isTruncated = IsTruncated || false;
           if (isTruncated) {
             console.log(`    More objects exist, fetching next batch.`);
@@ -93,14 +84,14 @@ await new Command()
         console.error(err);
       }
 
-      console.log("List objects complete.");
+      console.log(`Listing objects complete: ${objs.length} objects total.`);
       return objs;
     };
 
     const objs = await listObjs();
 
     console.log("Fetching object checksums...");
-    let sumResults = await Promise.all(
+    const sumResults = await Promise.all(
       objs.map((obj) =>
         (async () => {
           assert(obj.Key, "Missing object key");
@@ -118,7 +109,7 @@ await new Command()
             return null;
           }
           const resp = { key: obj.Key, sum: head.ChecksumSHA256 };
-          console.log(`    ${obj.Key}: ${head.ChecksumSHA256}`);
+          console.log(`    ${head.ChecksumSHA256}: ${obj.Key}`);
           return resp;
         })()
       )
@@ -128,5 +119,44 @@ await new Command()
     ) as any;
 
     console.log("Fetch object sums complete.");
+
+    console.log("Calculating and comparing checksums...");
+    let matches = 0;
+    let missing = 0;
+    let mismatches = 0;
+    await Promise.all(
+      sums.map(({ key, sum }) =>
+        (async () => {
+          const path = join(dir, key);
+
+          // Make sure the file exists
+          try {
+            await Deno.stat(path);
+          } catch (_) {
+            console.error(`File on S3 doesn't exist locally: ${key}`);
+            missing += 1;
+            return;
+          }
+
+          // Read file and compute the sum
+          const file = await Deno.readFile(path);
+          const localSum = sha256(file, undefined, "base64");
+          if (localSum != sum) {
+            console.error(`    Error: checksum mismatch: ${key}`);
+            console.error(`           S3   : ${sum}`);
+            console.error(`           Local: ${localSum}`);
+            mismatches += 1;
+          } else {
+            console.log(`    Match: ${key}`);
+            matches += 1;
+          }
+        })()
+      )
+    );
+    console.log(`Done comparing sums.`);
+    console.log(`    Total     : ${sums.length}`);
+    console.log(`    Matches   : ${matches}`);
+    console.log(`    Mismatches: ${mismatches}`);
+    console.log(`    Missing   : ${missing}`);
   })
   .parse(Deno.args);
